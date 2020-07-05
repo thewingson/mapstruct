b165a45758ba2d4b80c9d4a8f8de34a8fe1b443e
almatrakh97@gmail.com <almatrakh97@gmail.com>
2020-07-05 13:40:50 +0600

    Initial commit

---
+++ src/main/java/kz/almat/mapstruct/ArticleController.java
@@ -0,0 +1,9 @@
+package kz.almat.mapstruct;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/article")
+public class ArticleController {
+}
---
+++ src/test/java/kz/almat/mapstruct/MapstructApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.almat.mapstruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstructApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
